ca472c77561ed31ef9bd3659b5394a38ed057765
aditya-dawadikar <mail.aditya.dawadikar@gmail.com>
2025-02-24 16:19:51 -0800

    Add: Disable blog

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -21,7 +21,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "mail.aditya.dawadikar@gmail.com",
